eec81c28c7b44f72aa3e740a8e4ae7150223f577
Michał Kruszewski <kruszewski2@gmail.com>
2020-01-01 22:50:42 +0100

    Added sound device control to py script

--- set-yt-recording.py
+++ set-yt-recording.py
@@ -1,5 +1,13 @@
-def set_yt_recording():
-    pass
+import Settings
+import subprocess
 
-if __name__ == "__main___":
+def set_yt_recording():
+    try:
+        subprocess.run(["SoundVolumeView.exe", "/SetDefault", str(Settings.Name), str(Settings.DefaultDeviceType)])
+        subprocess.run(["SoundVolumeView.exe", "/SetVolume", str(Settings.Name), "50"])
+        subprocess.run(["SoundVolumeView.exe", "/Unmute", str(Settings.Name)])
+    except Exception as error:
+        print("An error occured: {0}".format(error))
+    
+if __name__ == "__main__":
     set_yt_recording()
