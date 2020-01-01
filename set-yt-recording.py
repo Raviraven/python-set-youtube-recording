c44931b37ccba3ffd9934565a509275193671990
Michał Kruszewski <kruszewski2@gmail.com>
2020-01-02 00:31:13 +0100

    Added OBSShortcutLocation

--- set-yt-recording.py
+++ set-yt-recording.py
@@ -3,9 +3,11 @@ import subprocess
 
 def set_yt_recording():
     try:
-        subprocess.run(["SoundVolumeView.exe", "/SetDefault", str(Settings.Name), str(Settings.DefaultDeviceType)])
-        subprocess.run(["SoundVolumeView.exe", "/SetVolume", str(Settings.Name), "50"])
-        subprocess.run(["SoundVolumeView.exe", "/Unmute", str(Settings.Name)])
+        # subprocess.run(["SoundVolumeView.exe", "/SetDefault", str(Settings.Name), str(Settings.DefaultDeviceType)])
+        # subprocess.run(["SoundVolumeView.exe", "/SetVolume", str(Settings.Name), "50"])
+        # subprocess.run(["SoundVolumeView.exe", "/Unmute", str(Settings.Name)])
+
+        subprocess.run([Settings.OBSShortcutLocation, ""])
     except Exception as error:
         print("An error occured: {0}".format(error))
     
