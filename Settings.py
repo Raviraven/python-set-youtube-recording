d7502aab3b67847b8c81d8da85c05fdb7671c233
Michał Kruszewski <kruszewski2@gmail.com>
2020-01-02 20:44:26 +0100

    Added audacity and microname

--- Settings.py
+++ Settings.py
@@ -4,3 +4,5 @@ Name="{0.0.0.00000000}.{684a852e-f754-4924-b6b7-ac57e084bf0a}"
 # 2 for communications
 DefaultDeviceType=1
 OBSShortcutLocation="C:\ProgramData\Microsoft\Windows\Start Menu\Programs\OBS Studio\OBS Studio (64bit).lnk"
+AudacityLocation=""
+MicroName=""
