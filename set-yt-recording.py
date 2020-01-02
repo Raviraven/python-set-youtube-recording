2be80d8f37a907409193e7e784d0726f4a659dd0
Michał Kruszewski <kruszewski2@gmail.com>
2020-01-02 20:49:34 +0100

    Added opening obs and audacity from cmd

--- set-yt-recording.py
+++ set-yt-recording.py
@@ -1,15 +1,22 @@
 import Settings
 import subprocess
+import os
 
 def set_yt_recording():
     try:
+        obs_path_converted = str("\"" + Settings.OBSShortcutLocation + "\"")
+        audacity_path_converted = str("\"" + Settings.AudacityLocation + "\"")
+
         # subprocess.run(["SoundVolumeView.exe", "/SetDefault", str(Settings.Name), str(Settings.DefaultDeviceType)])
         # subprocess.run(["SoundVolumeView.exe", "/SetVolume", str(Settings.Name), "50"])
         # subprocess.run(["SoundVolumeView.exe", "/Unmute", str(Settings.Name)])
 
-        subprocess.run([Settings.OBSShortcutLocation, ""])
+        # to change, only one works at one time
+        os.system(obs_path_converted)
+        os.system(audacity_path_converted)
     except Exception as error:
         print("An error occured: {0}".format(error))
-    
+
+
 if __name__ == "__main__":
     set_yt_recording()
