fc30bbbceab55cd85b9d9ef714cce216f67c4e4e
Michał Kruszewski <kruszewski2@gmail.com>
2020-01-03 20:54:54 +0100

    Changed device type to setting as default

--- Settings.py
+++ Settings.py
@@ -1,8 +1,8 @@
-SpeakersName="{0.0.0.00000000}.{dadcdff8-9daa-456e-9ad8-dcc4a6c2b28f}"
+SpeakersName="{0.0.0.00000000}.{ce4eb764-293d-486c-90c1-87cb3f50f55a}"
 MicroName="{0.0.1.00000000}.{73c9dd8e-ddcc-43d4-ba2d-9b3a91fc85c8}"
 # 0 for console
 # 1 for multimedia
 # 2 for communications
-DefaultDeviceType=2
+DefaultDeviceType=1
 OBSShortcutLocation=r"E:\Programy\OBS Win 10\obs-studio\bin\64bit\obs64.lnk"
 AudacityLocation=r"E:\Programy\Audacity Win10\\audacity.exe"
