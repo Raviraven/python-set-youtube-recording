c401af865f642b92490b775b528ef90eb869752f
Michał Kruszewski <kruszewski2@gmail.com>
2020-12-09 23:20:38 +0100

    Settings.py - cleared values

--- Settings.py
+++ Settings.py
@@ -1,11 +1,11 @@
-SpeakersName="{0.0.0.00000000}.{ce4eb764-293d-486c-90c1-87cb3f50f55a}"
-MicroName="{0.0.1.00000000}.{73c9dd8e-ddcc-43d4-ba2d-9b3a91fc85c8}"
+SpeakersName=""
+MicroName=""
 # 0 for console
 # 1 for multimedia
 # 2 for communications
 DefaultDeviceType=1
-OBSShortcutLocation=r"E:\Programy\OBS Win 10\obs-studio\bin\64bit\obs64.lnk"
-AudacityLocation=r"E:\Programy\Audacity Win10\\audacity.exe"
+OBSShortcutLocation=r""
+AudacityLocation=r""
 VolumeLevel=30
-TaskManagerLocation=r"C:\Windows\System32\Taskmgr.exe"
+TaskManagerLocation=r""
 RavBotekLocation=r"";
